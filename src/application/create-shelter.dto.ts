4e0eeccb155d1a3cc7f7cba5315872f59c3a36a5
alanmunoz <alanx015@hotmail.com>
2026-04-13 22:21:09 -0600

    add optional fields for creation

--- src/application/create-shelter.dto.ts
+++ src/application/create-shelter.dto.ts
@@ -1,32 +1,44 @@
-import { IsString } from 'class-validator';
+import { IsString, IsOptional } from 'class-validator';
 
 export class CreateShelterDto {
     @IsString()
     userOwnerId: string;
     @IsString()
     name: string;
+    @IsOptional()
     @IsString()
     description?: string;
+    @IsOptional()
     @IsString()
     phone?: string;
+    @IsOptional()
     @IsString()
     email?: string;
+    @IsOptional()
     @IsString()
     website?: string;
+    @IsOptional()
     @IsString()
     municipality?: string;
+    @IsOptional()
     @IsString()
     fullAddress?: string;
+    @IsOptional()
     @IsString()
     schedule?: string;
+    @IsOptional()
     @IsString()
     facebook?: string;
+    @IsOptional()
     @IsString()
     instagram?: string;
+    @IsOptional()
     @IsString()
     twitter?: string;
+    @IsOptional()
     @IsString()
     logo?: string;
+    @IsOptional()
     @IsString()
     imageUrl?: string;
 }
